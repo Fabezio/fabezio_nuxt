57b4d573ed983ea42b991780ae961217cf827d8e
fabezio <fabezio@outlook.fr>
2019-11-05 13:15:47 +0100

    ajout layout:home

---
+++ layouts/home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Navbar />
+    <!-- <div class="">
+      <p class="text-center">Intéressé? cliquez ici &rarr;
+        <a href="static/docs/cv.pdf" class="btn btn-link">
+          
+          Voir
+          </a>
+      </p>
+    </div>-->
+    <nuxt class="pb-5" />
+    <Footer />
+
+  </div>
+</template>
+
+<script>
+// import pdf from 'vue-pdf';
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style >
+
+</style>
